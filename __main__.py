e3d396be3167816c514015d50d2b25dfba30eb80
David Mc Ken <david.mcken@gmail.com>
2022-06-05 14:31:08 -0400

    Re-factored, need to test all functionality.

--- __main__.py
+++ __main__.py
@@ -1,115 +1,172 @@
-from sshtunnel import open_tunnel
-from time import sleep
-import sys
-import os
+'''
+Primary entrypoint for Docker container
+
+SSHTunneller is a simple docker image which allows you to establish SSH tunnels
+by just deploying a container with some enviroment variables by password or
+publickey authentication. The SSH Tunnels are pure python implemented with the
+SSHTunnel library. This little tool uses the magic of Docker to restart the
+tunnel if the tunnel dies for any reason.
+'''
+
+# System imports
 import ast
-import logging
 import io
+import logging
+import os
+import pprint
+import sys
+import time
+
 from contextlib import redirect_stdout
-#Logger
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%d/%m/%Y %H:%M:%S',level=logging.INFO)
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# External imports
+from sshtunnel import open_tunnel
 
-### Setup the console handler with a StringIO object
-log_capture_string = io.StringIO()
-ch = logging.StreamHandler(log_capture_string)
-ch.setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
 
-### Add the console handler to the logger
-logger.addHandler(ch)
+def check_log_level(level: str, default_level: str = 'INFO') -> int:
+    '''
+    Vet the logging level sent in, based on info from:
+    https://stackoverflow.com/questions/18846024/get-list-of-named-loglevels
 
+    Until a public / formal method exists for this the protected member access
+    will have to continue.
+    '''
+    # pylint: disable=protected-access
+    if level in logging._nameToLevel:
+        return logging._nameToLevel[level]
+    else:
+        return logging._nameToLevel[default_level]
 
-logging.info("Logger set!")
+def setup_logging() -> None:
+    '''
+    Setup logging for this script
+    '''
+    log_format = '%(asctime)s - %(name)s - %(levelname)s %(message)s'
+    date_format = '%Y-%m-%d %H:%M:%S' # ISO format
+    log_level_default = 'INFO'
 
-logging.info("SSH Tunnel starting...")
+    log_level = os.environ.get("log_level", log_level_default)
+    log_level_value = check_log_level(log_level, default_level=log_level_default)
 
-def main():
-    #Env variables
+    #TODO: allow the user to set any underlying modules / packages logging levels.
+    # Current thoughts on the matter, allow the definition of variable:
+    # log_level_<module name> = ERROR
+    # for example log_level_paramiko would then translate to:
+    # logging.getLogger('paramiko').setLevel(logging.ERROR)
+
+    logging.basicConfig(
+        format=log_format,
+        datefmt=date_format,
+        level=log_level_value,
+    )
+
+def parse_config() -> dict[str,str]:
+    '''
+    Parse config data.
+
+    Scan and parse environment and produce argument dictionary to pass to
+    sshtunnel's open_tunnel function.
+    '''
+    # Fetch and sanitise input
+    # TODO: fetch more on demand
     ssh_host = os.environ.get("ssh_host")
-    ssh_port = os.environ.get("ssh_port")
+    ssh_port = int(os.environ.get("ssh_port"))
     ssh_username = os.environ.get("ssh_username")
     ssh_password = os.environ.get("ssh_password")
     ssh_private_key_password = os.environ.get("ssh_private_key_password")
     remote_bind_addresses = os.environ.get("remote_bind_addresses")
     local_bind_addresses = os.environ.get("local_bind_addresses")
-    
-    private_key_dir = "/private.key"
-    if os.path.exists(private_key_dir):
+
+    tunnel_params = {}
+
+    # Mandatory parameters
+    tunnel_params['destination'] = (ssh_host, ssh_port)
+    tunnel_params['ssh_username'] = ssh_username
+    tunnel_params['set_keepalive'] = 30.0
+
+    private_key_file = "/private.key"
+    if os.path.exists(private_key_file):
         logging.info("Private key found, certificate mode enabled")
         if ssh_private_key_password is None:
-            logging.error("Private key password not set, please set it as an environment variable: ssh_private_key_password")
+            #TODO: allow no password but it must be set explicitly by setting
+            # the password variable to the string 'None'. 
+            logging.error("Private key password not set, please set it as "
+                "environment variable 'ssh_private_key_password'")
             sys.exit(-1)
-    elif ssh_password is None:
-        logging.error("SSH Password not provided, quitting...")
-        sys.exit(-1)
 
-    
-    
-
-    if os.path.exists(private_key_dir):
-        try:
-            with open_tunnel(
-            (ssh_host, int(ssh_port)),
-            ssh_username=ssh_username,
-            ssh_pkey=private_key_dir,
-            ssh_private_key_password=ssh_private_key_password,
-            remote_bind_addresses=ast.literal_eval(remote_bind_addresses),
-            local_bind_addresses=ast.literal_eval(local_bind_addresses),
-            set_keepalive=30.0
-            ) as server:
-                logging.info(f"SSH Tunnels established on {ssh_username}@{ssh_host}:  remote_bind_addresses:{remote_bind_addresses}, local_bind_addresses:{local_bind_addresses}")
-                while True:
-                    CheckTunnel(server,log_capture_string)
-            
-        except Exception as e:
-            logging.error("SSH Tunnel Failed!")
-            logging.error(e)
-        
-        finally:
-            logging.warning("SSH Tunnel ended")
+        tunnel_params['ssh_pkey'] = private_key_file
     else:
-        try:
-            with open_tunnel(
-            (ssh_host, int(ssh_port)),
-            ssh_username=ssh_username,
-            ssh_password=ssh_password,
-            remote_bind_addresses=ast.literal_eval(remote_bind_addresses),
-            local_bind_addresses=ast.literal_eval(local_bind_addresses),
-            set_keepalive=30.0
-            ) as server:
-                logging.info(f"SSH Tunnels established on {ssh_username}@{ssh_host}:  remote_bind_addresses:{remote_bind_addresses}, local_bind_addresses:{local_bind_addresses}")
-                while True:
-                    CheckTunnel(server,log_capture_string)
-            
-        except Exception as e:
-            logging.error("SSH Tunnel Failed!")
-            logging.error(e)
-        
-        finally:
-            logging.error("SSH Tunnel ended")
-            sys.exit(-2)
+        if ssh_password is None:
+            logging.error("SSH Password not provided, quitting...")
+            sys.exit(-1)
+        else:
+            tunnel_params['ssh_password'] = ssh_password
 
+    # Tunnel forwards
+    tunnel_params['remote_bind_addresses'] = ast.literal_eval(remote_bind_addresses)
+    tunnel_params['local_bind_addresses']  = ast.literal_eval(local_bind_addresses)
 
-def CheckTunnel(server, stdout):
+    return tunnel_params
+
+def main():
+    '''
+    Main function
+    '''
+    setup_logging()
+
+    ### Setup the console handler with a StringIO object
+    log_capture_string = io.StringIO()
+    ch = logging.StreamHandler(log_capture_string)
+    ch.setLevel(logging.ERROR)
+
+    ### Add the console handler to the logger
+    logger.addHandler(ch)
+
+    tunnel_config = parse_config()
+
+    logging.info("SSH Tunnel starting...")
+    logging.debug("SSH Tunnel parameters: %s", pprint.pformat(tunnel_config))
+
+    try:
+        with open_tunnel(
+            **tunnel_config
+        ) as server:
+            logging.info("SSH Tunnels established on %s@%s: "
+                "remote_bind_addresses: '%s', local_bind_addresses: '%s'",
+                tunnel_config['ssh_username'],
+                tunnel_config['ssh_host'],
+                tunnel_config['remote_bind_addresses'],
+                tunnel_config['local_bind_addresses'],
+            )
+            while True:
+                check_tunnel(server, log_capture_string)
+    except Exception as exc:
+        logging.error("SSH Tunnel Failed %s", exc)
+    finally:
+        logging.warning("SSH Tunnel ended")
+
+def check_tunnel(server, stdout):
+    '''
+    Check status of tunnel
+    '''
     #Check for remote side error from internal modules of Paramiko/SSHTunnel
     out = stdout.getvalue()
 
     if 'to remote side of the tunnel' in out:
-        logging.error("Problem with remote side, maybe the other side is unavailable, restarting...")
+        logging.error("Problem with remote side, maybe the other side is unavailable, "
+            "restarting...")
 
         sys.exit(-2)
 
     server.check_tunnels()
 
-    for x in server.tunnel_is_up.values():
-        if(x is False):
+    for ret in server.tunnel_is_up.values():
+        if ret is False:
             logging.error("Tunnel is dead, restarting...")
             sys.exit(-1)
-    sleep(1)
-    
+
+    time.sleep(1)
 
 if __name__== "__main__":
-   main()
+    main()
